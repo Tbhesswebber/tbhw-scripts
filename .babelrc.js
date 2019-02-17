87807a1f54d03bdecdaae65a3af014ac24f0e871
Tanner Hess Webber <tb@hesswebber.com>
2019-02-17 13:05:24 -0500

    chore(babel): correct babelrc.js configuration

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,15 @@
 const babel = {
-  presets: ['@babel/preset-stage-3', '@babel/preset-env'],
+  presets: [
+    '@babel/preset-stage-3',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: true,
+        },
+      },
+    ],
+  ],
 };
 
 module.exports = babel;
